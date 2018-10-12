4c8c251ffba10331650e4b5e4189e24c1c88ba67
Christinagithub <2605839246@qq.com>
2018-10-12 19:55:09 +0800

    Initial Commit

---
+++ test1-100/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  test1-100
+//
+//  Created by s20171105407 on 2018/10/12.
+//  Copyright © 2018年 s20171105407. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
